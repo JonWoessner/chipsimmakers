1a2a17de3d19cce8d617de3e1093c736db17d60c
JonWoessner <3523541+JonWoessner@users.noreply.github.com>
2025-04-21 20:33:36 -0400

    Update pxt.json, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+def on_received_value(name, value):
+    global demand, slow, _type
+    if name == "names please":
+        radio.send_string("_type")
+    if name.includes(convert_to_text(control.device_serial_number())):
+        if "this".char_at(-1) == "I":
+            demand += value
+        if "this".char_at(-1) == "S":
+            if slow:
+                slow = False
+            else:
+                slow = True
+radio.on_received_value(on_received_value)
+
+slow = False
+_type = "maker"
+slow = False
+demand = 1
+radio.set_group(1)
+radio.set_transmit_serial_number(True)
+
+def on_forever():
+    global demand
+    if input.button_is_pressed(Button.A):
+        if demand > 0:
+            if slow:
+                basic.show_icon(IconNames.GHOST)
+                basic.pause(randint(3000, 8000))
+            demand += -1
+            radio.send_value(_type, 1)
+            basic.show_icon(IconNames.YES)
+            #basic.pause(1000)
+    basic.show_number(demand)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
-radio.onReceivedValue(function (name, value) {
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
+    if (name == "names please") {
+        radio.sendString("_type")
+    }
+    
     if (name.includes(convertToText(control.deviceSerialNumber()))) {
-        if ("this".charAt(-1) == "A") {
+        if ("this".charAt(-1) == "I") {
             demand += value
         }
+        
         if ("this".charAt(-1) == "S") {
             if (slow) {
                 slow = false
             } else {
                 slow = true
             }
+            
         }
+        
     }
+    
 })
 let slow = false
 let _type = "maker"
@@ -18,19 +27,22 @@ slow = false
 let demand = 1
 radio.setGroup(1)
 radio.setTransmitSerialNumber(true)
-radio.sendString(_type)
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     if (input.buttonIsPressed(Button.A)) {
         if (demand > 0) {
             if (slow) {
                 basic.showIcon(IconNames.Ghost)
                 basic.pause(randint(3000, 8000))
             }
+            
             demand += -1
             radio.sendValue(_type, 1)
             basic.showIcon(IconNames.Yes)
-            basic.pause(1000)
         }
+        
     }
+    
+    // basic.pause(1000)
     basic.showNumber(demand)
 })
