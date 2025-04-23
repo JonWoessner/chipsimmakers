9b4131ee8cc8a1cbaf43f1bd39911e28838f6819
JonWoessner <3523541+JonWoessner@users.noreply.github.com>
2025-04-22 21:58:37 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,78 +1,114 @@
+"""
+
+i increase, d decrease, names please is init
+
+increase inventory for distributors
+
+"""
+def choose(arr: List[Array], maxi: number, curr: number):
+    # given a list of makers/suppliers, choose one that has the fewest current orders.
+    # can I also ensure that they rotate nicely??
+    curr += 1
+    if curr >= maxi:
+        curr = 0
+    arr[curr][1] += 1
+    return "" + str(arr[curr][0]) + "I"
+
 def on_button_pressed_a():
     radio.send_value("names please", 0)
     basic.show_icon(IconNames.HEART)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+# ## finds the index of a serial number
+def find(arr2: List[any], serial2: number):
+    i = 0
+    while i <= len(arr2) - 1:
+        if arr2[i][0] == serial2:
+            return i
+        i += 1
+    return -1
+
+def on_button_pressed_ab():
+    global started
+    started = True
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 def on_button_pressed_b():
     basic.show_icon(IconNames.SAD)
     for n in manufactlist:
-        radio.send_value(n[0]+"S", 1)
+        radio.send_value("" + str(n[0]) + "S", 1)
     for m in supplylist:
-        radio.send_value(m[0]+"S", 1)
+        radio.send_value("" + str(m[0]) + "S", 1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
-# i increase, d decrease, names please is init
+
+# if name.includes(convert_to_text(control.device_serial_number())):
+# basic.show_string("" + str(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)))
 
 def on_received_value(name, value):
-    global inventory, demand, manufactlist, supplylist, consumelist, current, count
+    global inventory, demand
     # ##### adding the players to lists
     if name == "name":
         if value == 1:
-            # suppliers
             count[0] += 1
             supplylist.append([radio.received_packet(RadioPacketProperty.SERIAL_NUMBER), 1])
             radio.send_value("" + str(supplylist[-1][0]) + "I", supplylist[-1][1])
         # add list
         if value == 2:
-            # manufacturers
             count[1] += 1
             manufactlist.append([radio.received_packet(RadioPacketProperty.SERIAL_NUMBER), 1])
             radio.send_value("" + str(manufactlist[-1][0]) + "I", manufactlist[-1][1])
         if value == 3:
-            # consumers
             count[2] += 1
             consumelist.append([radio.received_packet(RadioPacketProperty.SERIAL_NUMBER), 4])
             radio.send_value("" + str(consumelist[-1][0]) + "I", consumelist[-1][1])
     # #### End player init
-
     # if consumer demands
     if name == "consumer":
-        if inventory > 0:  
-            inventory += 0 - value  #update inventory, then tell that consumer that they were successful 
-            radio.send_value("" + str(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)) + "D", value)
-            loc = find(consumelist , radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)) #find consumer in list
-            if loc != -1:  #catch any errors 
+        if inventory > 0:
+            inventory += 0 - value
+            # update inventory, then tell that consumer that they were successful
+            radio.send_value("" + str(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)) + "D",
+                value)
+            loc = find(consumelist,
+                radio.received_packet(RadioPacketProperty.SERIAL_NUMBER))
+            # find consumer in list
+            if loc != -1:
                 consumelist[loc][1] -= 1
         else:
             demand = True
     # indicate more demand needed
-
     # ###suppliers
     if name == "supplier":
-        loc = find(supplylist , radio.received_packet(RadioPacketProperty.SERIAL_NUMBER))
+        loc = find(supplylist,
+            radio.received_packet(RadioPacketProperty.SERIAL_NUMBER))
         if loc != -1:
             supplylist[loc][1] -= 1
         # # decrement a supplier total and send to maker
         radio.send_value(choose(manufactlist, count[1], current[1]), 1)
     if name == "maker":
-        loc = find(manufactlist , radio.received_packet(RadioPacketProperty.SERIAL_NUMBER))
+        loc = find(manufactlist,
+            radio.received_packet(RadioPacketProperty.SERIAL_NUMBER))
         if loc != -1:
             manufactlist[loc][1] -= 1
-        inventory += 5  #increase inventory for distributors
-'''
-    if name.includes(convert_to_text(control.device_serial_number())):
-        basic.show_string("" + str(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)))'''
+        inventory += 5
 radio.on_received_value(on_received_value)
 
+timenow = 0
+demand = False
+started = False
+curr2 = 0
+_type = 0
 inventory = 0
+current: List[number] = []
+count: List[number] = []
+consumelist: List[List[number]] = []
+manufactlist: List[List[number]] = []
+supplylist: List[List[number]] = []
 # ############
 slow = False
-started = False
-count = [0,0,0]   ##suppliers, makers, consumers
+count = [0, 0, 0]
+# #suppliers, makers, consumers
 current = [0, 0, 0]
-demand = False
-supplylist: List[List[number]] = []
-manufactlist: List[List[number]] = []
-consumelist: List[List[number]] = []
 supplylist = [[0, 0]]
 manufactlist = [[0, 0]]
 consumelist = [[0, 0]]
@@ -86,39 +122,21 @@ stime = control.millis()
 _type = "distributor"
 radio.set_group(1)
 radio.set_transmit_serial_number(True)
-radio.send_string(_type)
-
-
-### finds the index of a serial number
-def find(arr: List[List[number]] , serial: int):
-    for i in range(len(arr)):
-        if arr[i][0] == serial:
-            return i
-    return -1
-
-def choose(arr: List[List[number]], maxi, curr ):
-    '''
-    given a list of makers/suppliers, choose one that has the fewest current orders.
-    can I also ensure that they rotate nicely??
-    '''
-    curr += 1
-    if curr >= maxi:
-        curr = 0
-    arr[curr][1] += 1
-    return arr[curr][0] + "I"
+radio.send_string("" + str((_type)))
 
 def on_forever():
+    global timenow, dtime, stime
     timenow = control.millis()
-    global stime, dtime, consumelist, started
     if started:
-        #send out additional demand every 4-9 seconds, random 1-2 each
+        # send out additional demand every 4-9 seconds, random 1-2 each
         if timenow - dtime > 5000:
+            dtime = timenow
             for x in consumelist:
-                val = randint(1,2)
-                radio.send_value(x[0]+'I', val)
+                val = randint(1, 2)
+                radio.send_value("" + str(x[0]) + "I", val)
                 x[1] += val
-        #if inventory drops, ping a supplier to make more stuff.
-        if inventory < 10 and ((timenow - stime) > 3000):
-            radio.send_value(choose(supplylist, count[0], current[0])+'I', 1)
-
+        # if inventory drops, ping a supplier to make more stuff.
+        if inventory < 10 and timenow - stime > 3000:
+            stime = timenow
+            radio.send_value("" + choose(supplylist, count[0], current[0]) + "I", 1)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,163 +1,145 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+/**
+ * i increase, d decrease, names please is init
+ * 
+ * increase inventory for distributors
+ */
+function choose (arr: Array[], maxi: number, curr: number) {
+    // given a list of makers/suppliers, choose one that has the fewest current orders.
+    // can I also ensure that they rotate nicely??
+    curr += 1
+    if (curr >= maxi) {
+        curr = 0
+    }
+    arr[curr][1] += 1
+return "" + arr[curr][0] + "I"
+}
+input.onButtonPressed(Button.A, function () {
     radio.sendValue("names please", 0)
     basic.showIcon(IconNames.Heart)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+// ## finds the index of a serial number
+function find (arr: any[], serial2: number) {
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if (arr[i][0] == serial2) {
+            return i
+        }
+    }
+    return -1
+}
+input.onButtonPressed(Button.AB, function () {
+    _type = 0
+})
+input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
     for (let n of manufactlist) {
-        radio.sendValue(n[0] + "S", 1)
+        radio.sendValue("" + n[0] + "S", 1)
     }
     for (let m of supplylist) {
-        radio.sendValue(m[0] + "S", 1)
+        radio.sendValue("" + m[0] + "S", 1)
     }
 })
-//  i increase, d decrease, names please is init
-// increase inventory for distributors
-/** 
-    if name.includes(convert_to_text(control.device_serial_number())):
-        basic.show_string("" + str(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)))
- */
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
+// if name.includes(convert_to_text(control.device_serial_number())):
+// basic.show_string("" + str(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)))
+radio.onReceivedValue(function (name, value) {
     let loc: number;
-    
-    //  ##### adding the players to lists
+// ##### adding the players to lists
     if (name == "name") {
         if (value == 1) {
-            //  suppliers
             count[0] += 1
-            supplylist.push([radio.receivedPacket(RadioPacketProperty.SerialNumber), 1])
-            radio.sendValue("" + ("" + supplylist[-1][0]) + "I", supplylist[-1][1])
+supplylist.push([radio.receivedPacket(RadioPacketProperty.SerialNumber), 1])
+            radio.sendValue("" + supplylist[-1][0] + "I", supplylist[-1][1])
         }
-        
-        //  add list
+        // add list
         if (value == 2) {
-            //  manufacturers
             count[1] += 1
-            manufactlist.push([radio.receivedPacket(RadioPacketProperty.SerialNumber), 1])
-            radio.sendValue("" + ("" + manufactlist[-1][0]) + "I", manufactlist[-1][1])
+manufactlist.push([radio.receivedPacket(RadioPacketProperty.SerialNumber), 1])
+            radio.sendValue("" + manufactlist[-1][0] + "I", manufactlist[-1][1])
         }
-        
         if (value == 3) {
-            //  consumers
             count[2] += 1
-            consumelist.push([radio.receivedPacket(RadioPacketProperty.SerialNumber), 4])
-            radio.sendValue("" + ("" + consumelist[-1][0]) + "I", consumelist[-1][1])
+consumelist.push([radio.receivedPacket(RadioPacketProperty.SerialNumber), 4])
+            radio.sendValue("" + consumelist[-1][0] + "I", consumelist[-1][1])
         }
-        
     }
-    
-    //  #### End player init
-    //  if consumer demands
+    // #### End player init
+    // if consumer demands
     if (name == "consumer") {
         if (inventory > 0) {
             inventory += 0 - value
-            // update inventory, then tell that consumer that they were successful 
-            radio.sendValue("" + ("" + radio.receivedPacket(RadioPacketProperty.SerialNumber)) + "D", value)
+            // update inventory, then tell that consumer that they were successful
+            radio.sendValue("" + radio.receivedPacket(RadioPacketProperty.SerialNumber) + "D", value)
             loc = find(consumelist, radio.receivedPacket(RadioPacketProperty.SerialNumber))
             // find consumer in list
             if (loc != -1) {
-                // catch any errors 
                 consumelist[loc][1] -= 1
             }
-            
         } else {
             demand = true
         }
-        
     }
-    
-    //  indicate more demand needed
-    //  ###suppliers
+    // indicate more demand needed
+    // ###suppliers
     if (name == "supplier") {
         loc = find(supplylist, radio.receivedPacket(RadioPacketProperty.SerialNumber))
         if (loc != -1) {
             supplylist[loc][1] -= 1
         }
-        
-        //  # decrement a supplier total and send to maker
+        // # decrement a supplier total and send to maker
         radio.sendValue(choose(manufactlist, count[1], current[1]), 1)
     }
-    
     if (name == "maker") {
         loc = find(manufactlist, radio.receivedPacket(RadioPacketProperty.SerialNumber))
         if (loc != -1) {
             manufactlist[loc][1] -= 1
         }
-        
         inventory += 5
     }
-    
 })
-let inventory = 0
-//  ############
-let slow = false
-let started = false
-let count = [0, 0, 0]
-// #suppliers, makers, consumers
-let current = [0, 0, 0]
+let timenow = 0
 let demand = false
-let supplylist : number[][] = []
-let manufactlist : number[][] = []
+let curr = 0
+let _type = 0
+let inventory = 0
+let current: number[] = []
+let count: number[] = []
 let consumelist : number[][] = []
+let manufactlist : number[][] = []
+let supplylist : number[][] = []
+// ############
+let slow = false
+count = [0, 0, 0]
+// #suppliers, makers, consumers
+current = [0, 0, 0]
 supplylist = [[0, 0]]
 manufactlist = [[0, 0]]
 consumelist = [[0, 0]]
 _py.py_array_pop(supplylist)
 _py.py_array_pop(manufactlist)
 _py.py_array_pop(consumelist)
-//  ## Starting inventory number
+// ## Starting inventory number
 inventory = 32
 let dtime = control.millis()
 let stime = control.millis()
-let _type = "distributor"
+_type = "distributor"
 radio.setGroup(1)
 radio.setTransmitSerialNumber(true)
-radio.sendString(_type)
-// ## finds the index of a serial number
-function find(arr: number[][], serial: number): number {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i][0] == serial) {
-            return i
-        }
-        
-    }
-    return -1
-}
-
-function choose(arr: number[][], maxi: number, curr: number): string {
-    /** 
-    given a list of makers/suppliers, choose one that has the fewest current orders.
-    can I also ensure that they rotate nicely??
-    
- */
-    curr += 1
-    if (curr >= maxi) {
-        curr = 0
-    }
-    
-    arr[curr][1] += 1
-    return arr[curr][0] + "I"
-}
-
-basic.forever(function on_forever() {
+radio.sendString("" + (_type))
+basic.forever(function () {
+    let started = 0
     let val: number;
-    let timenow = control.millis()
-    
+timenow = control.millis()
     if (started) {
         // send out additional demand every 4-9 seconds, random 1-2 each
         if (timenow - dtime > 5000) {
             for (let x of consumelist) {
                 val = randint(1, 2)
-                radio.sendValue(x[0] + "I", val)
+                radio.sendValue("" + x[0] + "I", val)
                 x[1] += val
             }
         }
-        
         // if inventory drops, ping a supplier to make more stuff.
         if (inventory < 10 && timenow - stime > 3000) {
-            radio.sendValue(choose(supplylist, count[0], current[0]) + "I", 1)
+            radio.sendValue("" + choose(supplylist, count[0], current[0]) + "I", 1)
         }
-        
     }
-    
 })
