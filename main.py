2dd0fa380be6b34a92927bad0a94d18e160cb80a
JonWoessner <3523541+JonWoessner@users.noreply.github.com>
2025-04-27 20:16:05 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,50 +1,42 @@
 def on_received_value(name, value):
-    global demand, disp, slow, _type
-    if name == "names please":
+    global demand, disp, bought
+    if name == "init":
         basic.pause(randint(100, 3000))
         radio.send_value("name", 3)
-    if name.includes(convert_to_text(control.device_serial_number())):
-        if "this".char_at(-1) == "I":
-            demand += value
-            disp = "needed"
-        if "this".char_at(-1) == "D":
-            demand += 0 - value
-            disp = "success"
-        if "this".char_at(-1) == "S":
-            if slow:
-                slow = False
-            else:
-                slow = True
+    if value == control.device_serial_number():
+        if name == 'I':
+            bought += 1
 radio.on_received_value(on_received_value)
 
-slow = False
 disp = ""
 disp = "needed"
-_type = "consumer"
-slow = False
 demand = 0
+bought = 0
 radio.set_group(1)
 radio.set_transmit_serial_number(True)
 
 def on_forever():
     global disp
     if input.button_is_pressed(Button.A):
-        if demand > 0:
-            # if slow:
-            # basic.show_icon(IconNames.GHOST)
-            # basic.pause(randint(3000, 8000))
-            # set flag
-            disp = "try"
-            # send radio request
-            radio.send_value(_type, 1)
-    # display update section
-    if disp == "try":
-        basic.show_icon(IconNames.TARGET)
-        disp = "needed"
-    if disp == "success":
-        basic.show_icon(IconNames.YES)
-        disp = "needed"
-    if disp == "needed":
-        basic.show_number(demand)
-    disp = ""
+        disp = "try"
+        # send radio request
+        radio.send_value('consumer', 1)
+    
 basic.forever(on_forever)
+
+
+def onIn_background():
+    global disp
+    while True:
+        # display update section
+        if disp == "try":
+            basic.show_icon(IconNames.TARGET)
+            disp = "needed"
+        if disp == "success":
+            basic.show_icon(IconNames.YES)
+            disp = "needed"
+        if disp == "needed":
+            basic.show_number(demand)
+        disp = ""
+        basic.pause(2)
+control.in_background(onIn_background)
--- main.ts
+++ main.ts
@@ -1,70 +1,52 @@
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
     
-    if (name == "names please") {
+    if (name == "init") {
         basic.pause(randint(100, 3000))
         radio.sendValue("name", 3)
     }
     
-    if (name.includes(convertToText(control.deviceSerialNumber()))) {
-        if ("this".charAt(-1) == "I") {
-            demand += value
-            disp = "needed"
-        }
-        
-        if ("this".charAt(-1) == "D") {
-            demand += 0 - value
-            disp = "success"
-        }
-        
-        if ("this".charAt(-1) == "S") {
-            if (slow) {
-                slow = false
-            } else {
-                slow = true
-            }
-            
+    if (value == control.deviceSerialNumber()) {
+        if (name == "I") {
+            bought += 1
         }
         
     }
     
 })
-let slow = false
 let disp = ""
 disp = "needed"
-let _type = "consumer"
-slow = false
 let demand = 0
+let bought = 0
 radio.setGroup(1)
 radio.setTransmitSerialNumber(true)
 basic.forever(function on_forever() {
     
     if (input.buttonIsPressed(Button.A)) {
-        if (demand > 0) {
-            //  if slow:
-            //  basic.show_icon(IconNames.GHOST)
-            //  basic.pause(randint(3000, 8000))
-            //  set flag
-            disp = "try"
-            //  send radio request
-            radio.sendValue(_type, 1)
+        disp = "try"
+        //  send radio request
+        radio.sendValue("consumer", 1)
+    }
+    
+})
+control.inBackground(function onIn_background() {
+    
+    while (true) {
+        //  display update section
+        if (disp == "try") {
+            basic.showIcon(IconNames.Target)
+            disp = "needed"
         }
         
+        if (disp == "success") {
+            basic.showIcon(IconNames.Yes)
+            disp = "needed"
+        }
+        
+        if (disp == "needed") {
+            basic.showNumber(demand)
+        }
+        
+        disp = ""
+        basic.pause(2)
     }
-    
-    //  display update section
-    if (disp == "try") {
-        basic.showIcon(IconNames.Target)
-        disp = "needed"
-    }
-    
-    if (disp == "success") {
-        basic.showIcon(IconNames.Yes)
-        disp = "needed"
-    }
-    
-    if (disp == "needed") {
-        basic.showNumber(demand)
-    }
-    
-    disp = ""
 })
